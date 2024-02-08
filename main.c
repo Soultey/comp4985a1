f71f9753a913737d52f0a2406cbc66437edd3da7
Soultey <alexanderjgibbison@gmail.com>
2024-02-07 18:23:40 -0800

    alex build modular, get and parsing

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <arpa/inet.h>
-// #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-// #include <ndbm.h> database header
 
 #define BUFFER_SIZE 1024
 #define TEN 10
 #define FIVE 5
+#define HTML_FILE "/Users/alex/Documents/comp4981a1/comp4985a1/index.html"
 
 #ifndef SOCK_CLOEXEC
     #pragma GCC diagnostic push
@@ -17,12 +17,24 @@
     #pragma GCC diagnostic pop
 #endif
 
-void handle_client(int client_socket);
+// Function prototypes
 void start_server(const char *server_ip, int server_port) __attribute__((noreturn));
+int  create_server_socket(const char *server_ip, int server_port);
+void accept_client_connections(int server_socket) __attribute__((noreturn));
+void handle_client(int client_socket);
+void send_file(int client_socket);
 
+// Start the server
 void start_server(const char *server_ip, int server_port)
 {
-    long               port = server_port;
+    int server_socket = create_server_socket(server_ip, server_port);
+    printf("Server listening on port %d...\n", server_port);
+    accept_client_connections(server_socket);
+}
+
+// Create and bind server socket
+int create_server_socket(const char *server_ip, int server_port)
+{
     int                server_socket;
     struct sockaddr_in server_address;
 
@@ -37,7 +49,7 @@ void start_server(const char *server_ip, int server_port)
     // Initialize server_address struct
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family      = AF_INET;
-    server_address.sin_port        = htons((uint16_t)port);
+    server_address.sin_port        = htons((uint16_t)server_port);
     server_address.sin_addr.s_addr = inet_addr(server_ip);
 
     // Bind the server socket to the server_address
@@ -54,13 +66,19 @@ void start_server(const char *server_ip, int server_port)
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %ld...\n", port);
+    return server_socket;
+}
 
+// Accept client connections
+void accept_client_connections(int server_socket)
+{
     while(1)
     {
         int                client_socket;
         struct sockaddr_in client_address;
-        socklen_t          client_address_len = sizeof(client_address);
+        socklen_t          client_address_len;
+
+        client_address_len = sizeof(client_address);
 
         // Accept a client connection
         client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len);
@@ -72,24 +90,20 @@ void start_server(const char *server_ip, int server_port)
 
         printf("Client connected\n");
 
+        // Handle client request
         handle_client(client_socket);
     }
 }
 
+// Handle client request
 void handle_client(int client_socket)
 {
-    const char *response = "HTTP/1.0 200 OK\r\nContent-Length: 12\r\n\r\nHello, World!";
-    ssize_t     bytes_sent;
-
-    // Receive data from client
     char    buffer[BUFFER_SIZE];
-    ssize_t bytes_received = read(client_socket, buffer, sizeof(buffer) - 1);
+    ssize_t bytes_received;
 
-    if(bytes_received > 0)
-    {
-        buffer[bytes_received] = '\0';    // Null-terminate the string
-    }
-    else
+    bytes_received = read(client_socket, buffer, sizeof(buffer) - 1);
+
+    if(bytes_received <= 0)
     {
         // Handle read error or closed socket
         printf("Nothing received from client\n");
@@ -97,20 +111,77 @@ void handle_client(int client_socket)
         return;
     }
 
+    // Null-terminate the received data
+    buffer[bytes_received] = '\0';
+
     // Print the received HTTP request
     printf("Received HTTP request:\n%s\n", buffer);
 
-    // Respond to the client
-    bytes_sent = write(client_socket, response, strlen(response));
-    if(bytes_sent == -1)
+    // Check if it's a GET request
+    if(strstr(buffer, "GET") == buffer)
     {
-        perror("Error sending response to client");
+        send_file(client_socket);
+    }
+    else
+    {
+        // Unsupported HTTP method, send a 501 Not Implemented response
+        const char *not_implemented_response = "HTTP/1.0 501 Not Implemented\r\n\r\n";
+        ssize_t     bytes_sent               = write(client_socket, not_implemented_response, strlen(not_implemented_response));
+        if(bytes_sent == -1)
+        {
+            perror("Error sending response to client");
+        }
     }
 
     // Close the client socket
     close(client_socket);
 }
 
+// Send HTML file to client
+void send_file(int client_socket)
+{
+    const char *response_header = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    ssize_t     bytes_sent      = write(client_socket, response_header, strlen(response_header));
+    char        html_buffer[BUFFER_SIZE];
+    ssize_t     html_bytes_read;
+    FILE       *html_file;
+
+    if(bytes_sent == -1)
+    {
+        perror("Error sending response to client");
+        close(client_socket);
+        return;
+    }
+
+    // Open the HTML file
+
+    html_file = fopen(HTML_FILE, "re");    // Open the file here
+
+    if(html_file == NULL)
+    {
+        perror("Error opening HTML file");
+        close(client_socket);
+        return;
+    }
+
+    // Read and send the HTML file contents
+
+    while((html_bytes_read = (ssize_t)fread(html_buffer, 1, sizeof(html_buffer), html_file)) > 0)
+    {
+        bytes_sent = write(client_socket, html_buffer, (size_t)html_bytes_read);
+        if(bytes_sent == -1)
+        {
+            perror("Error sending response to client");
+            fclose(html_file);
+            close(client_socket);
+            return;
+        }
+    }
+
+    fclose(html_file);
+}
+
+// Main function
 int main(int argc, const char *argv[])
 {
     long        port;
